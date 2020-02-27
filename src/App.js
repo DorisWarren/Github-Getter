c26e687e68f2693198db75571942ce34f67114c5
DorisWarren <doriswarren11@gmail.com>
2020-02-27 14:29:50 -0800

    Found Github search API––tested it out made sure it worked (Will be using Search Repositories on Developer Guide )

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "./App.css";
 
 
+// https://api.github.com/search/repositories?q=doris
+
 function App() {
   return (
     <div>
-      <form onSubmit={(evt) => {
-        evt.preventDefault()
-        console.log("onSubmit took place")
+      <form onSubmit={evt => {
+        evt.preventDefault();
+        console.log(evt.target.elements.query.value);
       }}>
         <input 
           type="text" 
