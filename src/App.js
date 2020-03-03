bf21d3e5a0e05d05ca0b57a889e832a754733e71
DorisWarren <doriswarren11@gmail.com>
2020-03-02 18:07:30 -0800

    add star_gazer count and description

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import "./App.css";
-
+import { TiStar} from 'react-icons/ti';
+import { IoMdSearch} from 'react-icons/io';
 
 
 function App() {
   const [inputValue, setInputValue] = React.useState("");
-  const [repos, setRepos] = React.useState([])
+  const [repos, setRepos] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState (false);
+  const [error, setError] = React.useState (false);
  
   React.useEffect(() => {
     if (!inputValue) {
       return;
     }
+    setIsLoading(true);
 
     // API Call 
 
@@ -20,34 +24,66 @@ function App() {
       })
       .then(data => {
         console.log(data);
+        setIsLoading(false);
         setRepos(data.items)
-      });
+    }).catch(err => {
+        setIsLoading(false);
+        setError(true);
+        console.log(err);
+    });
   }, [inputValue])
 
 console.log(repos);
 
   return (
-    <div>
-      <form onSubmit={evt => {
-        evt.preventDefault();
-        setInputValue(evt.target.elements.query.value);
-      }}>
-        <input 
-          type="text" 
-          name="query" 
-          placeholder="Search Github Repositories"
-        />
-      </form>
-      <ul>
-        {repos.map( repo => {
-          return <li key={repo.id}>
-            <a href={repo.html_url}>{repo.name}</a>
-            <p>{repo.description}</p>
-          </li>;
-        })}
-      </ul>
+    <div className="Container">
+      <div className="wrapper">
+        <form onSubmit={evt => {
+          evt.preventDefault();
+          setInputValue(evt.target.elements.query.value);
+        }}>
+       
+        <i class="fas fa-search"></i>
+          <input 
+            type="text" 
+            name="query" 
+            className="github_search_bar"
+            placeholder="Search Github Repositories"
+            autoComplete="off"
+          />
+        </form>
+        {isLoading && <div>Searching...</div>}
+        {error && (
+          <div>
+            Unexpected Error Occurred. Please Try again
+          </div>)}
+       
+            <ul className="repo_list">
+              {repos.map((repo) => {
+            
+                return (
+                  <li className="repo_items"key={repo.id}>
+                    <a href={repo.html_url} target="_blank" className="list_details">
+                    {repo.name}
+                    </a>
+                    
+                    <p><TiStar/>{repo.stargazers_count} </p>
+                    <p>{repo.description}</p>
+                    {/* {repo.owner.map((owner) => ( 
+                      <p key={owner.id}> 
+                      <p>{owner.id}</p>{owner.login}
+                      </p>))} */}
+                    
+                  </li>
+      
+                );
+              })}
+            </ul>
+         
+        </div>     
     </div>
   );
 }
+
 
 export default App;
