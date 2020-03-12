787d9374cfb02cbfef48484ac6bc0ce2cb46a600
DorisWarren <doriswarren11@gmail.com>
2020-03-12 13:25:58 -0700

    cleaned up some of the UL tags for divs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./App.scss";
 import Header from "./Header";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faStar, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faStar } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
@@ -33,49 +33,63 @@ function App() {
   return (
     <div className="Container">
       <Header />
-          {/* <div className="wrapper"> */}
-            <form onSubmit={evt => {
-              evt.preventDefault();
-              setInputValue(evt.target.elements.query.value);
-            }}>
-            <FontAwesomeIcon className="search_icon"icon={faSearch} />
-              <input 
-                type="text" 
-                name="query" 
-                className="github_search_bar"
-                placeholder="Search Keyword..."
-                autoComplete="off"
-              />
+        <div className="form-container">
+          <form onSubmit={evt => {
+                evt.preventDefault();
+                setInputValue(evt.target.elements.query.value);
+              }}>
+              <FontAwesomeIcon className="search_icon"icon={faSearch} />
+                <input 
+                  type="text" 
+                  name="query" 
+                  className="search_bar"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
             </form>
-              {isLoading && <div className="searching">Searching...</div>}
-              {error && (
-                <div>
-                  Unexpected Error Occurred. Please Try again
-                </div>)}
+              </div>
+                <div className="result-container">
+                  <div className="isLoading">
+                  {isLoading && <div className="searching">Searching...</div>}
+                    {error && (
+                      <div>
+                        Unexpected Error Occurred. Please Try again
+                      </div>)}
+                  </div>
+              <div className="result-list">
             
-                <ul className="repo_list">
-                  {repos.sort((a, b) => {
-                    if (a.stargazers_count < b.stargazers_count) return 1
-                    else if (a.stargazers_count > b.stargazers_count) return -1
-                    return 0
-                  }).map((repo) => {         
-                    return (
-                      <li className="repo_items"key={repo.id}>
-                        <div className="row_one_details"> 
-                          <a href={repo.html_url} target="_blank" className="repo_name">
-                          {repo.full_name}<p>{repo.language}</p></a>
-                          <p><FontAwesomeIcon className="star" icon={faStar}/>
-                          {repo.stargazers_count}</p>
+                    {repos.sort((a, b) => {
+                      if (a.stargazers_count < b.stargazers_count) return 1
+                      else if (a.stargazers_count > b.stargazers_count) return -1
+                      return 0
+                    }).map((repo) => {         
+                      return (
+                        <div className="repo_items"key={repo.id}>
+                          <div className="info">
+                            <div className="box">
+                              <div>
+                                <a href={repo.html_url} target="_blank" className="link">{repo.full_name}</a>
+                              </div>
+                              <div>
+                                <FontAwesomeIcon className="star" icon={faStar}/>{repo.stargazers_count}
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="info">
+                            <p className="description">{repo.description}</p>
+                          </div>
+
                         </div>
-                          <p className="description">{repo.description}</p>
-                      </li>
-                    );
-                  })}
-                </ul>
-        {/* </div>      */}
+                      );
+                    })}
+                  </div>
+                </div>
     </div>
   );
 }
 
 
 export default App;
+
+
